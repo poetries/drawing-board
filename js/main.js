1e02c57af076a4995a14cfb8ff4b6864dafdcd31
poetries <jingguanliuye@gmail.com>
2016-12-03 20:23:55 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-//lineType show
+
 
 $(function(){
+	
+	//lineType show
 	$("#header ul.tool li.line").hover(function(){
 		$(this).find(".lineType").show();
 	},function(){
 		$(this).find(".lineType").hide();
+	});
+	
+	//button active
+	$("#header ul.tool li").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+	});
+	$("#header ul.tool .lineType p").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
 	});
 
 });
